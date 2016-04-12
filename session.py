d645198f7142300050420f39330725a418af0879
Til Boerner <tilman.boerner@gmx.net>
2016-04-12 23:24:40 +0200

    Improve output

--- session.py
+++ session.py
@@ -18,13 +18,14 @@ quarter_hours = sorted(
 
 count_hours = lambda qts: sum(1 for _ in qts) / 4
 
-by_day = lambda dt: dt.date().isoformat()
+by_day = lambda dt: dt.date().isoformat() + ' ' + dt.strftime('%a')
 by_week = lambda dt: '{0} W{1:02}'.format(*dt.isocalendar())
 by_month = lambda dt: '{dt.year}-{dt.month:02}'.format(dt=dt)
 
 group_to_dict = lambda grp: {key: count_hours(qts) for key, qts in grp}
 
 today = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+week_start = today - timedelta(days=today.weekday())  # Monday=0, Sunday=6
 
 print('Months')
 months = groupby(quarter_hours, by_month)
@@ -32,14 +33,12 @@ pprint(group_to_dict(months))
 
 print()
 print('Weeks')
-this_month = today.replace(day=1)
-weeks = groupby((q for q in quarter_hours if q >= this_month), by_week)
+six_weeks_ago = week_start - timedelta(days=7*6)
+weeks = groupby((q for q in quarter_hours if q >= six_weeks_ago), by_week)
 pprint(group_to_dict(weeks))
 
 print()
 print('Days')
-days_into_week = today.weekday()  # Mon=0, ..., Sun=6
-this_week = today - timedelta(days=days_into_week)
-last_week = this_week - timedelta(days=7)
+last_week = week_start - timedelta(days=7)
 days = groupby((q for q in quarter_hours if q >= last_week), by_day)
 pprint(group_to_dict(days))
